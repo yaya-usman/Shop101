4816cd7242bcc9f53b7e73f566252715486e2cd1
yaya-usman <yayaazeez222@gmail.com>
2022-02-08 07:32:57 +0300

    footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div>&nbsp;</div>
+        <div>
+          &copy; {new Date().getFullYear()} Shop101 | Made with ❤️
+        </div>
+        <Link href={"/"}>
+          <i className="fab fa-github"></i>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Navbar.module.css";
